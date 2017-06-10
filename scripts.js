765b4fcfd438358a8391c6f1faa21d4d5cc15513
Emanuel <eecastro1011@gmail.com>
2017-06-09 18:00:00 -0700

    can create shape objects with string keys and fret number values, working on distance parameters

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 /**
  * Created by emanuel on 5/24/17.
  */
+//For createChord()
 var notesArray = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
 var stringsArray = ['E', 'A', 'D', 'G', 'B', 'E'];
 
+//For createChord() and findShape()
 var major = [0, 4, 7];
 var minor = [0, 3, 7];
 var diminished = [0, 3, 6];
@@ -18,7 +20,40 @@ var dom7 = [0, 4, 7, 10];
 var rootnote;
 var quality;
 var shapeDistance = 0;
-var shapeDictionary = {};
+var shapesDictionary = {};
+
+var tempArray = [];
+var sixthString = [];
+var fifthString = [];
+var fourthString = [];
+var thirdString = [];
+var secondString = [];
+var firstString = [];
+
+tempArray.push(document.getElementsByClassName('six'));
+sixthString = tempArray[0];
+tempArray = [];
+
+tempArray.push(document.getElementsByClassName('five'));
+fifthString = tempArray[0];
+tempArray = [];
+
+tempArray.push(document.getElementsByClassName('four'));
+fourthString = tempArray[0];
+tempArray = [];
+
+tempArray.push(document.getElementsByClassName('three'));
+thirdString = tempArray[0];
+tempArray = [];
+
+tempArray.push(document.getElementsByClassName('two'));
+secondString = tempArray[0];
+tempArray = [];
+
+tempArray.push(document.getElementsByClassName('one'));
+firstString = tempArray[0];
+tempArray = [];
+
 
 resetColors();
 resetOptions();
@@ -93,53 +128,29 @@ function findShape() {
     //This function basically searches the "strings" and lights up the ones that are in the spellingArray
     resetColors();
     var spellingArray = createChord();
-    var tempArray = [];
-    var sixthString = [];
-    var fifthString = [];
-    var fourthString = [];
-    var thirdString = [];
-    var secondString = [];
-    var firstString = [];
     var currentChoice;
 
-    tempArray.push(document.getElementsByClassName('six'));
-    sixthString = tempArray[0];
-    tempArray = [];
-
-    tempArray.push(document.getElementsByClassName('five'));
-    fifthString = tempArray[0];
-    tempArray = [];
-
-    tempArray.push(document.getElementsByClassName('four'));
-    fourthString = tempArray[0];
-    tempArray = [];
-
-    tempArray.push(document.getElementsByClassName('three'));
-    thirdString = tempArray[0];
-    tempArray = [];
-
-    tempArray.push(document.getElementsByClassName('two'));
-    secondString = tempArray[0];
-    tempArray = [];
-
-    tempArray.push(document.getElementsByClassName('one'));
-    firstString = tempArray[0];
-    tempArray = [];
-
+    shapesDictionary["shape1"] = shapeObject();
     while (spellingArray.length > 0) {
         for (var i = 0; i < sixthString.length; i++) {
             if (sixthString[i].innerHTML == spellingArray[0]) {
+                // This next line is for determining the distance of the notes in the strings
+                // if ((Math.abs(parseInt(sixthString[i].id.slice(2)) - shapesDictionary["shape1"][6])) > 3) {
+                //     break;
+                // } else {
+                //
+                // }
                 currentChoice = sixthString[i];
+                shapesDictionary["shape1"][6] = parseInt(currentChoice.id.slice(2));
                 currentChoice.style.backgroundColor = "red";
                 spellingArray.splice(0, 1);
-                //This console.log actually logs the id name
-                console.log(sixthString[i].id);
                 break;
             }
         }
         for (var j = 0; j < fifthString.length; j++) {
             if (fifthString[j].innerHTML == spellingArray[0]) {
                 currentChoice = fifthString[j];
+                shapesDictionary["shape1"][5] = parseInt(currentChoice.id.slice(2));
                 currentChoice.style.backgroundColor = "red";
                 spellingArray.splice(0, 1);
                 break;
@@ -148,6 +159,7 @@ function findShape() {
         for (var k = 0; k < fourthString.length; k++) {
             if (fourthString[k].innerHTML == spellingArray[0]) {
                 currentChoice = fourthString[k];
+                shapesDictionary["shape1"][4] = parseInt(currentChoice.id.slice(2));
                 currentChoice.style.backgroundColor = "red";
                 spellingArray.splice(0, 1);
                 break;
@@ -156,6 +168,7 @@ function findShape() {
         for (var l = 0; l < thirdString.length; l++) {
             if (thirdString[l].innerHTML == spellingArray[0]) {
                 currentChoice = thirdString[l];
+                shapesDictionary["shape1"][3] = parseInt(currentChoice.id.slice(2));
                 currentChoice.style.backgroundColor = "red";
                 spellingArray.splice(0, 1);
                 break;
@@ -164,6 +177,7 @@ function findShape() {
         for (var m = 0; m < secondString.length; m++) {
             if (secondString[m].innerHTML == spellingArray[0]) {
                 currentChoice = secondString[m];
+                shapesDictionary["shape1"][2] = parseInt(currentChoice.id.slice(2));
                 currentChoice.style.backgroundColor = "red";
                 spellingArray.splice(0, 1);
                 break;
@@ -172,12 +186,14 @@ function findShape() {
         for (var n = 0; n < firstString.length; n++) {
             if (firstString[n].innerHTML == spellingArray[0]) {
                 currentChoice = firstString[n];
+                shapesDictionary["shape1"][1] = parseInt(currentChoice.id.slice(2));
                 currentChoice.style.backgroundColor = "red";
                 spellingArray.splice(0, 1);
                 break;
             }
         }
     }
+    console.log(shapesDictionary);
     resetOptions();
 }
 
@@ -190,3 +206,15 @@ function resetOptions(){
     document.getElementById('root').selectedIndex = 0;
     document.getElementById('quality').selectedIndex = 0;
 }
+
+function shapeObject(){
+    //This function returns a blank template that is meant to be used as a value for a key in the shapesDictionary
+    return {6:"", 5:"", 4:"", 3:"", 2:"", 1:""};
+}
+
+//Playing with a way to create shape objects to include in the shapesDictionary
+// for (var i = 0; i < 5; i++){
+//     shapesDictionary["shape" + String(i)] = shapeObject();
+// }
+//
+// console.log(shapesDictionary);
